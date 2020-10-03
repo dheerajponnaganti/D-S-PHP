b107f0b02f433b6350100703db1e525b17070f09
Dheeraj P <pdheeraj2998@gmail.com>
2020-10-03 14:04:40 +0530

    Signup and Signin frames done

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    require "header.php"; 
+?>
+
+<!-- Main -->
+<main>
+      <!-- Owl Carousel -->
+      <section id="banner-area">
+        <div class="owl-carousel owl-theme">
+          <div class="item">
+            <img src="./assets/img1.jpg" alt="Banner1" width="100vw" />
+          </div>
+          <div class="item">
+            <img src="./assets/img2.jpg" alt="Banner2" />
+          </div>
+          <div class="item">
+            <img src="./assets/img3.jpg" alt="Banner3" />
+          </div>
+        </div>
+      </section>
+      <!-- Owl Carousel End -->
+    </main>
+    <!-- Main End -->
+
+<?php
+    require "footer.php"; 
+?>
